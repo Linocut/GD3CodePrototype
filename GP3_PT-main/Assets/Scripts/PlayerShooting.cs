58720a41f32e8a1baef589754f17fa21a65b4370
ecortes15 <ecortes15@knights.ucf.edu>
2023-03-29 12:58:32 -0400

    Started on audio

--- GP3_PT-main/Assets/Scripts/PlayerShooting.cs
+++ GP3_PT-main/Assets/Scripts/PlayerShooting.cs
@@ -12,25 +12,37 @@ using UnityEngine;
     //shooting
     private GameObject shot;
     public float shotSpeed; 
+    AudioSource audioSource;
+    public AudioSource gunShot;
 
     void Start()
     {
-    
+        gunShot = GetComponent<AudioSource>();
     }
 
+    
+    
     void Update()
     {
         if (Input.GetButtonDown("Shoot"))
+        {
             transform.LookAt(orientation);
             PlayerShoot();
+            gunShot.Play();
+
+        }
     }
 
-    void PlayerShoot(){
+   
+
+    void PlayerShoot()
+    {
         //mouse and keyboard
         if (Input.GetButtonDown("Shoot"))
         {
             shot = Instantiate(bullet,transform.position,transform.rotation) as GameObject;
             shot.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward*shotSpeed);
+
         }    
     }
 }
