f74d0ccbed28d11480b1756da3212925f11fdd6d
Jasmine May Darman <65880499+Linocut@users.noreply.github.com>
2023-04-08 02:55:41 -0400

    npcInteractable

---
+++ GP3_PT-main/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public float interactRange;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+            foreach(Collider collider in colliderArray)
+            {
+                if (collider.TryGetComponent(out NPCInteractable npcInteractable))
+                {
+                    npcInteractable.Interact();
+                }
+            }
+        }
+        
+        
+    }
+}
